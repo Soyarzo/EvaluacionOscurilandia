69040c0dca2e996fd929e8a377508ad7391595fa
JosueQuiriban <josuealejandropq@gmail.com>
2020-02-24 18:23:11 -0300

    Clase tablero

---
+++ ProyectoOscurilandia/src/cl/awakelab/clases/Tablero.java
@@ -0,0 +1,17 @@
+package cl.awakelab.clases;
+
+import java.util.ArrayList;
+
+import cl.awakelab.evaluacion.Carro;
+
+public class Tablero {
+
+	private Carro[] carros;
+
+	public Tablero() {
+
+		Carro[] carros = new Carro[18];
+
+		ArrayList Huevo[];
+	}
+}
