375d8102871e89ef160fb7b31e070eb6e6f4c798
jleivas <60983887+jleivas@users.noreply.github.com>
2020-02-24 18:15:39 -0300

    primer desarrollo clase carro

--- ProyectoOscurilandia/src/cl/awakelab/clases/Carro.java
+++ ProyectoOscurilandia/src/cl/awakelab/clases/Carro.java
@@ -1,5 +1,47 @@
 package cl.awakelab.clases;
 
 public class Carro {
+	
+	int cantidadOcupantes;
+	String fechaIngreso;
+	int[] posicionTablero;
+	
+	public Carro() {
+		this.posicionTablero = new int[2];
+		
+	}
+
+	public int getCantidadOcupantes() {
+		return cantidadOcupantes;
+	}
+
+	public void setCantidadOcupantes(int cantidadOcupantes) {
+		this.cantidadOcupantes = cantidadOcupantes;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public int[] getPosicionTablero() {
+		return posicionTablero;
+	}
+
+	public void setPosicionTablero(int alto,int ancho) {
+		 this.posicionTablero[0]= alto;
+		 this.posicionTablero[1]= ancho;
+	}
+
+	public abstract String modelo();
+
+	@Override
+	public String toString() {
+		return "Carro [cantidadOcupantes=" + cantidadOcupantes + ", fechaIngreso=" + fechaIngreso + ", posicionTablero="
+				+ posicionTablero + ", modelo()=" + modelo() + "]";
+	}
 
 }
