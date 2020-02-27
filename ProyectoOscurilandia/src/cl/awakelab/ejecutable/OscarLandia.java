f105498dd950d1e0217a8731310f7307c07976d9
JosueQuiriban <josuealejandropq@gmail.com>
2020-02-26 23:51:30 -0300

    avance JDOc

--- ProyectoOscurilandia/src/cl/awakelab/ejecutable/OscarLandia.java
+++ ProyectoOscurilandia/src/cl/awakelab/ejecutable/OscarLandia.java
@@ -2,6 +2,11 @@ package cl.awakelab.ejecutable;
 
 import cl.awakelab.clases.Menu;
 
+/**
+ * Esta es la clase main, contiene el metodo Menu.
+ * @author Manuel Soto, Josue Quiriban, Javier Leiva
+ *
+ */
 public class OscarLandia {
 
 	public static void main(String[] args) {
