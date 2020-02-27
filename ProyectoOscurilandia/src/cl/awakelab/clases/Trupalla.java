33a52ef6d7b1bc7c3e51eafeae0c47923cfb3f46
JosueQuiriban <josuealejandropq@gmail.com>
2020-02-26 23:30:08 -0300

    Actualizacion JDoc

--- ProyectoOscurilandia/src/cl/awakelab/clases/Trupalla.java
+++ ProyectoOscurilandia/src/cl/awakelab/clases/Trupalla.java
@@ -1,10 +1,26 @@
 package cl.awakelab.clases;
 
+/**
+ * Clase Trupalla hereda los atributos y metodos de clase padre Carro, tiene
+ * los atributos armadura y conductor.
+ * @author Manuel Soto, Josue Quiriban, Javier Leiva
+ *
+ */
 public class Trupalla extends Carro {
 
+	/**
+	 * @param armadura Se refiere al nivel de armadura que tiene el carro,
+	 * este esta definido por un azar.
+	 * @param conductor Se refiere al nombre completo 
+	 * del conductor del vehiculo.
+	 */
 	private int armadura;
 	private String conductor;
 
+	/**
+	 * Constructor por defecto que le da valor por azar a armadura , a el
+	 * conductor y al modelo de Carro.
+	 */
 	public Trupalla() {
 		super();
 		RandomData azares = new RandomData();
@@ -14,6 +30,7 @@ public class Trupalla extends Carro {
 		setModelo("Trupalla");
 	}
 
+	//Metodos Get y Set.
 	public int getArmadura() {
 		return armadura;
 	}
