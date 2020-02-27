0f555a7c2b79271453a2d564bb3623b108762caa
JosueQuiriban <josuealejandropq@gmail.com>
2020-02-26 23:21:32 -0300

    Avance JavaDoc

--- ProyectoOscurilandia/src/cl/awakelab/clases/Caguano.java
+++ ProyectoOscurilandia/src/cl/awakelab/clases/Caguano.java
@@ -1,10 +1,26 @@
 package cl.awakelab.clases;
 
+/**
+ * Clase Caguano hereda de la superClase Carro. Esta clase 
+ * tiene como atributos alcanceDeTiro y colorConfeti.
+ * @author Manuel Soto, Josue Quiriban, Javier Leiva
+ *
+ */
 public class Caguano extends Carro {
-
+	
+	/**
+	 * @param alcanceDeTiro este atributo define el
+	 *  alcance de tiro del carro Caguano.
+	 * @param colorConfeti Este atributo indica el color
+	 * de confeti que lanza el carro Caguano. 
+	 */
 	private int alcanceDeTiro;
 	private String colorConfeti;
-
+	
+	/**
+	 * Este es el constructor por defecto el cual por medio
+	 * del azar define el color de confeti y el alcance de tiro.
+	 */
 	public Caguano() {
 		super();
 		RandomData azares = new RandomData();
@@ -14,6 +30,7 @@ public class Caguano extends Carro {
 		setModelo("Caguano");
 	}
 
+	//Metodos Get y Set.
 	public float getAlcanceDeTiro() {
 		return alcanceDeTiro;
 	}
--- ProyectoOscurilandia/src/cl/awakelab/clases/Carro.java
+++ ProyectoOscurilandia/src/cl/awakelab/clases/Carro.java
@@ -2,16 +2,37 @@ package cl.awakelab.clases;
 
 import java.util.Arrays;
 
-
+/**
+ * Esta es la clase padre del sistema, de ella heredaran clases hijas con los 
+ * atributos cantidadOcupantes, fechaIngreso, posicionTablero, modelo, fila, 
+ * columna.
+ * @author Manuel Soto, Josue Quiriban, Javier Leiva
+ *
+ */
 public class Carro {
 
+	//atributos
 	private int cantidadOcupantes;
 	private String fechaIngreso;
 	private int[] posicionTablero = new int[2];
 	private String modelo;
 	private int fila;
 	private int columna;
-
+	/**
+	 * @param cantidadOcupantes Este atributo indica
+	 *  la cantidad de ocupantes que tiene un carro.
+	 * @param fechaIngreso Este atributo se refiere a la
+	 *  fecha de ingreso del ocupante a la institucion.
+	 * @param posicionTablero Este atributo indica la posicion en el arreglo.
+	 * @param modelo Este atributo indica el modelo de carro creado.
+	 * @param fila Indica la posicion en el arreglo.
+	 * @param columna Indica la posicion en el arreglo.
+	 */
+	
+	/**
+	 * Este es el constructor por defecto que le da el valor
+	 *  por defecto a los atributos de la clase.
+	 */
 	public Carro() {
 		RandomData azares = new RandomData();
 		setPosicionTablero(getFila(), getColumna());
@@ -20,7 +41,8 @@ public class Carro {
 		setFila();
 		setColumna();
 	}
-
+	
+	//Metodos Get y Set.
 	public int getFila() {
 		return fila;
 	}
@@ -70,7 +92,8 @@ public class Carro {
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-
+	
+	//Metodo toString
 	@Override
 	public String toString() {
 		return "Carro [cantidadOcupantes=" + cantidadOcupantes + ", fechaIngreso=" + fechaIngreso + ", posicionTablero="
